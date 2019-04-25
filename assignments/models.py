89865e71f48e3699d5f4f27992e65dc5b5405ba1
JAshMe <mailme.jyotmehta@gmail.com>
2019-04-25 06:00:37 +0530

    Assignments Models Completed

--- assignments/models.py
+++ assignments/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
+from ASP import settings
+import os
 from django.contrib.auth import get_user_model
 
 
@@ -11,6 +13,37 @@ def user_directory_path(instance, filename):
     :return: String : Path of the File to be saved
     """
     return str(instance.user.id) + "/" + str(filename)
+
+
+class Environment(models.Model):
+    """
+    This model will contain all the environments and their bash files
+    """
+
+    ENVIRONMENT_CHOICES = (
+        ('ap-sp', 'Apache Spark'),
+        ('yn-mp', 'Yarn/Map Reduce'),
+        ('hb', 'HBase'),
+    )
+
+    env_id = models.CharField(
+        verbose_name="Environment",
+        choices=ENVIRONMENT_CHOICES,
+        default='ap-sp',
+        blank=False,
+        max_length=10,
+        primary_key=True
+    )
+
+    bash_file_url = models.FilePathField(
+        path=os.path.join(settings.MEDIA_ROOT, "bash_files"),
+        recursive=False,
+        allow_files=True,
+        allow_folders=False,
+    )
+
+    def __str__(self):
+        return self.get_env_id_display()
 
 
 class Assignment(models.Model):
@@ -26,8 +59,29 @@ class Assignment(models.Model):
         upload_to=user_directory_path,
         help_text="Upload .zip Files Only",
         blank=False,
-        validators=[FileExtensionValidator(allowed_extensions=["zip"],message="Only '.zip' files are allowed!!")]
+        validators=[FileExtensionValidator(allowed_extensions=["zip"], message="Only '.zip' files are allowed!!")]
     )
+    run_command = models.CharField(
+        verbose_name="Run Command",
+        max_length=500,
+        help_text="Enter the command for running your project"
+    )
+    is_eval = models.BooleanField(
+        verbose_name="Is Evaluated",
+        default=False
+    )
+
+    user = models.ForeignKey(verbose_name="Student", to=get_user_model(), on_delete=models.CASCADE)
+
+    env = models.ForeignKey(verbose_name="Environment Selected", to=Environment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + " - " + self.user.username
+
+
+
+
+
 
 
 
